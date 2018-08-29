aeb1902ae36e9f93f03243ea6139718d6c276b04
Kyle Coburn <kylecoburn@gmail.com>
2018-08-29 10:41:23 -0700

    🐛 Comment out upcoming rules

--- vue.js
+++ vue.js
@@ -11,20 +11,20 @@ module.exports = {
 	rules: {
 		'vue/attribute-hyphenation': ['error', 'never'],
 		'vue/attributes-order': 'off',
-		'vue/component-name-in-template-casing': ['error', 'PascalCase', {
-			ignores: [
-				'v-separate',
-			],
-		}],
+		// 'vue/component-name-in-template-casing': ['error', 'PascalCase', {
+		// 	ignores: [
+		// 		'v-separate',
+		// 	],
+		// }],
 		'vue/html-indent': 'off',
 		'vue/max-attributes-per-line': ['error', {
 			singleline: 999,
 			multiline: 0,
 		}],
-		'vue/no-spaces-around-equal-signs-in-attribute': 'error',
+		// 'vue/no-spaces-around-equal-signs-in-attribute': 'error',
 		'vue/script-indent': ['error', 'tab'],
-		'vue/singleline-html-element-content-newline': ['error', {
-			ignoreWhenNoAttributes: true,
-		}],
+		// 'vue/singleline-html-element-content-newline': ['error', {
+		// 	ignoreWhenNoAttributes: true,
+		// }],
 	},
 }
