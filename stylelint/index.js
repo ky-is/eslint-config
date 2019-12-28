f6294a1aa8b9017e182b80485730cce754830be7
Kyle Coburn <kylecoburn@gmail.com>
2019-12-28 15:00:00 -0800

    ✨🌱 Add stylelint support

---
+++ stylelint/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	extends: 'stylelint-config-recommended',
+	rules: {
+		'indentation': 'tab',
+		'no-invalid-double-slash-comments': false,
+		'number-leading-zero': 'always',
+	},
+}
