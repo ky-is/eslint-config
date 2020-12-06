2c17058a04b58da71302825b1c1353eb1801e679
Kyle Coburn <kylecoburn@gmail.com>
2020-12-06 09:35:30 -0700

    ✨ add separate TS rules

---
+++ typescript.js
@@ -0,0 +1,9 @@
+module.exports = {
+	extends: [
+		'@ky-is',
+	],
+
+	rules: {
+		'@typescript-eslint/consistent-type-imports': 'warn',
+	},
+}
--- vue-typescript.js
+++ vue-typescript.js
@@ -1,5 +1,6 @@
 module.exports = {
 	extends: [
+		'@ky-is/eslint-config/typescript',
 		'@ky-is/eslint-config/vue',
 		'@vue/typescript', // Requires `npm i -D @vue/eslint-config-typescript`
 	],
